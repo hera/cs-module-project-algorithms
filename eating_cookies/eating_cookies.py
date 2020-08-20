3f7d0db1dc359168d5f8b6b928eeb2a7a0b5636f
hera <a.heraimenka@gmail.com>
2020-08-20 11:39:36 -0700

    Save prev cookie version

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,6 +3,15 @@ Input: an integer
 Returns: an integer
 '''
 
+"""
+def eating_cookies(n):
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    else:
+        return eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
+"""
 
 def eating_cookies(n, cache=None):
     if n < 0:
